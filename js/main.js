062ca3d3728e4f78ed604c5eaa8a163cd99b09d1
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-04-06 17:05:37 +1100

    Add Main Game Loop
    Added Players
    Drawing To Board
    New Game Button
    Check for Win
    Check for Game Over

---
+++ js/main.js
@@ -0,0 +1,143 @@
+function createPlayer(name) {
+    let playerName = name;
+    const setPlayerName = (newName) => playerName = newName;
+
+    let symbol = name;
+    const getSymbol = () => symbol;
+
+    return { playerName, getSymbol, setPlayerName };
+}
+
+function createScore(name) {
+    const playerName = name;
+    let gamesWon = 1;
+    const incrementScore = () => { gamesWon++ }
+    return { playerName, gamesWon, incrementScore };
+}
+
+const scoreboard = (function () {
+    const scores = [];
+    const AddScore = (name) => {
+        const findPlayerIndex = scores.findIndex(name);
+        if (findPlayer === -1) {
+            scores.push(createScore(name));
+        } else {
+            scores[findPlayerIndex].incrementScore();
+        }
+    }
+    const GetScore = (name) => scores.find(name).gamesWon;
+    return { AddScore, GetScore };
+})()
+
+const Gameboard = (function () {
+    let gameOver = false;
+    let player1Won = false;
+    let player2Won = false;
+    const maxTurns = 9;
+    let currentTurn = 1;
+
+    const player1 = createPlayer('x');
+    const player2 = createPlayer('o');
+
+    let currentPlayer = player1;
+
+    const Cells = Array(9).fill("");
+
+    const checkWin = (currentSymbol) => {
+        const combinations =
+            [[0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [6, 4, 2],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]]
+
+        let winCondition = false;
+        // theoretically... for each sub array -> return true if all subarray elements in reference to Cells i.e. Cells[SubArrayElement] Equals Current Symbol
+        combinations.forEach((element) => {
+            if (element.every((subElement) => Cells[subElement] === currentSymbol)) {
+                winCondition = true;
+            }
+        })
+        return winCondition;
+    }
+
+    const GetCurrentPlayer = () => currentPlayer;
+
+    const EndTurn = () => {
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+        currentTurn++
+    }
+
+    const emptyCell = (cell) => {
+        if (Cells[cell] === "") {
+            return true;
+        }
+        return false;
+    }
+
+    const drawBoard = () => {
+        [...document.querySelectorAll('.cell')].forEach((gameCell, iterator) => {
+            gameCell.innerHTML = Cells[iterator]
+        })
+    }
+
+    const GetRound = () => currentTurn;
+
+    const PlayRound = (cellPicked) => {
+        if (currentTurn > maxTurns) {
+            gameOver = true;
+        }
+
+        if (gameOver) {
+            alert("Game is Over, Restart Game");
+            return;
+        }
+
+        if (!emptyCell(cellPicked)) {
+            return;
+        }
+
+        Cells[cellPicked] = currentPlayer.getSymbol();
+        drawBoard();
+
+        player1Won = checkWin('x');
+        if (player1Won) console.log("Player 1 Won");
+        player2Won = checkWin('o');
+        if (player2Won) console.log("Player 2 Won")
+
+        if (player1Won || player2Won) {
+            gameOver = true;
+        }
+
+        EndTurn();
+    }
+
+    const ResetGame = () => {
+        gameOver = false;
+        currentTurn = 1;
+        currentPlayer = player1;
+        player1Won = false;
+        player2Won = false;
+        Cells.fill("");
+        drawBoard();
+    }
+
+    return { GetCurrentPlayer, PlayRound, ResetGame, GetRound }
+})()
+
+const gridCells = [...document.querySelectorAll('.cell')]
+
+gridCells.forEach((gameCell, iterator) => {
+    gameCell.addEventListener("click", () => {
+        Gameboard.PlayRound(iterator);
+    })
+})
+
+document.getElementsByClassName('new-game-btn')[0].addEventListener("click", () => Gameboard.ResetGame());
